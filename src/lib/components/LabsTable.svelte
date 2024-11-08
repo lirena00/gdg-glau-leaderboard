4ac0b87b4178e02ec4487cfef2b11a0ef6a41fe7
lirena <sakshamkushwaha12776@gmail.com>
2024-11-08 19:42:48 +0530

    add search icon component and LabsTable with course links; update Header styles

---
+++ src/lib/components/LabsTable.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table/index.js';
+	import Card from '$lib/components/ui/card/card.svelte';
+
+	interface Lab {
+		name: string;
+		url: string;
+	}
+
+	const labs: Lab[] = [
+		{
+			name: 'The Basics of Google Cloud Compute',
+			url: 'https://www.cloudskillsboost.google/course_templates/754?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Get Started with Cloud Storage',
+			url: 'https://www.cloudskillsboost.google/course_templates/725?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Get Started with Pub/Sub',
+			url: 'https://www.cloudskillsboost.google/course_templates/728?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Get Started with API Gateway',
+			url: 'https://www.cloudskillsboost.google/course_templates/662?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Get Started with Looker',
+			url: 'https://www.cloudskillsboost.google/course_templates/647?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Get Started with Dataplex',
+			url: 'https://www.cloudskillsboost.google/course_templates/726?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Get Started with Google Workspace Tools',
+			url: 'https://www.cloudskillsboost.google/course_templates/676?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Cloud Functions: 3 Ways',
+			url: 'https://www.cloudskillsboost.google/course_templates/696?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'App Engine: 3 Ways',
+			url: 'https://www.cloudskillsboost.google/course_templates/671?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Cloud Speech API: 3 Ways',
+			url: 'https://www.cloudskillsboost.google/course_templates/700?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Monitoring in Google Cloud',
+			url: 'https://www.cloudskillsboost.google/course_templates/747?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Networking Fundamentals on Google Cloud',
+			url: 'https://www.cloudskillsboost.google/course_templates/748?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Analyze Images with the Cloud Vision API',
+			url: 'https://www.cloudskillsboost.google/course_templates/633'
+		},
+		{
+			name: 'Prompt Design in Vertex AI',
+			url: 'https://www.cloudskillsboost.google/course_templates/976?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		},
+		{
+			name: 'Develop GenAI Apps with Gemini and Streamlit',
+			url: 'https://www.cloudskillsboost.google/course_templates/978'
+		},
+		{
+			name: 'Diwali in the Arcade',
+			url: 'https://www.cloudskillsboost.google/games/5628?utm_source=gcaf-site&utm_medium=website&utm_campaign=arcade-facilitator24'
+		}
+	];
+</script>
+
+<Card
+	class="w-full overflow-hidden rounded-lg border-none bg-gradient-to-br from-gray-800 to-gray-900 md:w-3/4"
+>
+	<Table.Root class=" border-slate-300 text-gray-200">
+		<Table.Header>
+			<Table.Row class="border-gray-500 ">
+				<Table.Head class="text-center text-gray-100">Labs To Be Completed</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			{#each labs as lab}
+				<Table.Row class="border-gray-500 ">
+					<Table.Cell
+						><a class="text-[#63b3ed] hover:underline" href={lab.url} target="_blank">{lab.name}</a
+						></Table.Cell
+					>
+				</Table.Row>
+			{/each}
+		</Table.Body>
+	</Table.Root>
+</Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Header from '$lib/components/Header.svelte';
+	import LabsTable from '$lib/components/LabsTable.svelte';
+
 	import Papa from 'papaparse';
 
 	import * as Table from '$lib/components/ui/table/index.js';
 
 	import Card from '$lib/components/ui/card/card.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
-	import GdLogo from '$lib/components/icons/gd_logo.svelte';
+	import Search from '$lib/components/icons/search.svelte';
 	import Trophy from '$lib/components/icons/trophy.svelte';
 	import Award from '$lib/components/icons/award.svelte';
 
 	interface DataRow {
 		'User Name': string;
-		'User Email': string;
 		'Google Cloud Skills Boost Profile URL': string;
 		'Profile URL Status': string;
 		'Access Code Redemption Status': string;
@@ -47,10 +48,10 @@
 	}
 </script>
 
-<div class="max-w-screen min-h-screen bg-gradient-to-br from-gray-900 to-gray-800">
+<div class="max-w-screen dark min-h-screen bg-gradient-to-br from-gray-900 to-gray-800">
 	<Header />
-	<div class="flex flex-col gap-8 p-4">
-		<div class=" grid gap-4 md:grid-cols-2">
+	<div class="flex flex-col items-center justify-center gap-8 p-4">
+		<div class=" grid w-full justify-center gap-4 md:w-3/4 md:grid-cols-2">
 			<Card class="border-none bg-gradient-to-br from-gray-800 to-gray-900 p-6">
 				<div class="flex items-center gap-4">
 					<div class="rounded-lg bg-green-900 p-3">
@@ -58,7 +59,9 @@
 					</div>
 					<div>
 						<p class="text-sm font-medium text-gray-400">Eligible Participants for swags</p>
-						<p class="text-3xl font-bold text-gray-100">80</p>
+						<p class="text-3xl font-bold text-gray-100">
+							{data.filter((row) => row['All Skill Badges & Games Completed'] == 'Yes').length}
+						</p>
 					</div>
 				</div>
 			</Card>
@@ -74,18 +77,19 @@
 				</div>
 			</Card>
 		</div>
-		<div class="relative">
+		<div class="relative w-full md:w-3/4">
+			<Search class="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground " />
 			<Input
 				type="text"
 				placeholder="Search participants by name..."
 				bind:value={searchQuery}
 				on:input={filterData}
-				class="rounded-lg border-none bg-gray-800 pl-10 text-gray-100 placeholder-gray-500 focus:border-none"
+				class="rounded-lg border-none bg-gray-800 pl-10 text-gray-100 placeholder-gray-500 focus-visible:ring-offset-1"
 			/>
 		</div>
 
 		<Card
-			class="overflow-hidden rounded-lg border-none bg-gradient-to-br from-gray-800 to-gray-900"
+			class="w-full overflow-hidden rounded-lg border-none bg-gradient-to-br from-gray-800 to-gray-900 md:w-3/4"
 		>
 			<Table.Root class=" border-slate-300 text-gray-200">
 				<Table.Header>
@@ -97,25 +101,29 @@
 						>
 						<Table.Head class="text-center text-gray-100">No. of Skill Badges Completed</Table.Head>
 						<Table.Head class="text-center text-gray-100">No. of Arcade Games Completed</Table.Head>
-						<Table.Head class="text-center text-gray-100"
-							>Names of Completed Arcade Games</Table.Head
-						>
 					</Table.Row>
 				</Table.Header>
 				<Table.Body>
-					{#each filteredData as row}
-						<Table.Row class=" border-gray-500 disabled:pointer-events-none">
-							<Table.Cell>{row['User Name']}</Table.Cell>
-							<Table.Cell>{row['Access Code Redemption Status']}</Table.Cell>
+					{#each filteredData.sort((a, b) => b['# of Skill Badges Completed'] - a['# of Skill Badges Completed']) as row}
+						<Table.Row class="border-gray-500 disabled:pointer-events-none ">
+							<Table.Cell
+								><a href={row['Google Cloud Skills Boost Profile URL']} target="_blank"
+									>{row['User Name']}</a
+								>
+								{#if row['All Skill Badges & Games Completed'] == 'Yes'}
+									<Trophy class="ml-1 inline-block h-4 w-4 text-yellow-500" />
+								{/if}
+							</Table.Cell>
+							<Table.Cell class="text-center">{row['Access Code Redemption Status']}</Table.Cell>
 							<Table.Cell>{row['All Skill Badges & Games Completed']}</Table.Cell>
 							<Table.Cell>{row['# of Skill Badges Completed']}</Table.Cell>
 							<Table.Cell>{row['# of Arcade Games Completed']}</Table.Cell>
-							<Table.Cell>{row['Names of Completed Arcade Games']} ss</Table.Cell>
 						</Table.Row>
 					{/each}
 				</Table.Body>
 			</Table.Root>
 		</Card>
+		<LabsTable />
 	</div>
 </div>
 
